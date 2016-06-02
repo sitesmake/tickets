1f1d50c7e704326e49ca2cc344cf617985111f7a
Sites Make <sitesmake@gmail.com>
2016-06-02 15:51:41 +0300

    stations and train scaffolds

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :trains
+  resources :railway_stations
+
+  root 'railway_stations#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
