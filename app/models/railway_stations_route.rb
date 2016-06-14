d80f948c72927bd6625ec39d06ff27b0131c1446
Sites Make <sitesmake@gmail.com>
2016-06-14 21:47:00 +0300

    stations by position scope

--- app/models/railway_stations_route.rb
+++ app/models/railway_stations_route.rb
@@ -4,4 +4,6 @@ class RailwayStationsRoute < ActiveRecord::Base
 
   validates :railway_station_id, uniqueness: { scope: :route_id }
   validates :position, uniqueness: { scope: :route_id }
+
+  scope :by_position, -> { order(:position) }
 end
